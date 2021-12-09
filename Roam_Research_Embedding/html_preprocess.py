2f03437ecd060da5bcec526fd64d3542fedb9f98
GStechschulte <stechschulteg@gmail.com>
2021-12-09 18:59:04 +0100

    hand written preprocess rules

--- Roam_Research_Embedding/html_preprocess.py
+++ Roam_Research_Embedding/html_preprocess.py
@@ -15,7 +15,7 @@ class html_preprocess():
         read_in = 0
         not_wrote = 0
 
-        for html in dir[:5]:
+        for html in dir:
             with open(self.path + html, 'r') as f:
                 try:
                     self.html_clean = self.soup(f.read())
@@ -38,6 +38,11 @@ class html_preprocess():
 
         souped = BeautifulSoup(html_content, 'html.parser')
         data = souped.get_text().replace('\n', '. ')
+        data = data.replace('__', '')
+        data = data.replace('|', '')
+        data = data.replace('[[]]', '')
+        data = data.replace('- via', '')
+        data = data.replace('Tags:', '')
         
         return data
 
@@ -46,9 +51,3 @@ if __name__ == '__main__':
     
     path = '/Users/wastechs/Documents/data/roam_clean/'
     html_preprocess(path).read_content()
-
-
-
-      
-
-    
